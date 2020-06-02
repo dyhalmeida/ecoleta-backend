e0dd37e72edde39abc0e2dfac93bd7c287560cf6
Diego Almeida <dyhalmeida@gmail.com>
2020-06-02 10:48:31 -0300

    Added knex migrations

---
+++ knexfile.ts
@@ -0,0 +1,15 @@
+import { resolve } from 'path';
+/**
+ * command to run migrations
+ * npx knex migrate:latest --knexfile knexfile.ts migrate:latest
+ */
+module.exports = {
+  client: 'sqlite3',
+  connection: {
+    filename: resolve(__dirname, 'src', 'database', 'database.sqlite'),
+  },
+  migrations: {
+    directory: resolve(__dirname, 'src', 'database', 'migrations')
+  },
+  useNullAsDefault: true,
+};
