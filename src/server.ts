d49f807c1e684703ca46475020d3f1b261c3bf37
Diego Almeida <dyhalmeida@gmail.com>
2020-06-02 11:15:17 -0300

    Added configuration to serve static files

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import routes from './routes';
+import { resolve } from 'path';
 
 const app = express();
 
 app.use(routes);
+app.use('/uploads', express.static(resolve(__dirname, '..', 'uploads')));
 
 app.listen(3333);
