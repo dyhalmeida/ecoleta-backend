0020b0e715be6911e8550701d0cc54ae77881575
Diego Almeida <dyhalmeida@gmail.com>
2020-06-02 11:35:16 -0300

    Added controller and route to list all items

---
+++ src/controllers/itemController.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from 'express';
+import knex from '../database/connection';
+
+class ItemsController {
+
+  async index(request: Request, response: Response) {
+
+    const items = await knex('items').select('*');
+    const serializeItems = items.map(item => {
+      return {
+        id: item.id,
+        title: item.title,
+        image: item.image,
+        image_url: `http://localhost:3333/uploads/${item.image}`
+      }
+    });
+
+    return response.json(serializeItems);
+  }
+
+}
+
+export default new ItemsController();
--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,10 @@
 import express from 'express';
+import ItemController from './controllers/itemController';
+
 const routes = express.Router();
 
 routes.get('/', (request, response) => response.json({ message: 'E-coleta API running' }));
+
+routes.get('/items', ItemController.index);
 
 export default routes;
