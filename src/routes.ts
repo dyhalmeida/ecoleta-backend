946c3a344df2cc49602adeda905187e05587f8e0
Diego Almeida <dyhalmeida@gmail.com>
2020-06-07 09:48:55 -0300

    added multer to upload image

--- src/routes.ts
+++ src/routes.ts
@@ -1,15 +1,21 @@
-import express from 'express';
-import ItemController from './controllers/itemController';
-import PointController from './controllers/pointController';
+import express from "express";
+import multer from "multer";
+import multerConfig from "./config/multer";
+
+import ItemController from "./controllers/itemController";
+import PointController from "./controllers/pointController";
 
 const routes = express.Router();
+const upload = multer(multerConfig);
 
-routes.get('/', (request, response) => response.json({ message: 'E-coleta API running' }));
+routes.get("/", (request, response) =>
+  response.json({ message: "E-coleta API running" })
+);
 
-routes.get('/items', ItemController.index);
+routes.get("/items", ItemController.index);
 
-routes.post('/points', PointController.store);
-routes.get('/points', PointController.index);
-routes.get('/points/:id', PointController.show);
+routes.post("/points", upload.single("image"), PointController.store);
+routes.get("/points", PointController.index);
+routes.get("/points/:id", PointController.show);
 
 export default routes;
