bb541cfa4a38c636f7ac0b4f2244a21f18242b88
Diego Almeida <dyhalmeida@gmail.com>
2020-06-07 11:37:45 -0300

    added celebrate validation

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import { celebrate, Joi } from "celebrate";
+
 import multer from "multer";
 import multerConfig from "./config/multer";
 
@@ -14,7 +16,27 @@ routes.get("/", (request, response) =>
 
 routes.get("/items", ItemController.index);
 
-routes.post("/points", upload.single("image"), PointController.store);
+routes.post(
+  "/points",
+  upload.single("image"),
+  celebrate(
+    {
+      body: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.string().required(),
+        city: Joi.string().required(),
+        uf: Joi.string().required().max(2),
+        latitutde: Joi.number().required(),
+        longitude: Joi.number().required(),
+        items: Joi.string().required(),
+      }),
+    },
+    { abortEarly: false }
+  ),
+  PointController.store
+);
+
 routes.get("/points", PointController.index);
 routes.get("/points/:id", PointController.show);
 
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,16 @@
-import express from 'express';
-import cors from 'cors';
-import routes from './routes';
-import { resolve } from 'path';
+import express from "express";
+import cors from "cors";
+import routes from "./routes";
+import { resolve } from "path";
+import { errors } from "celebrate";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
-app.use('/uploads', express.static(resolve(__dirname, '..', 'uploads')));
+app.use("/uploads", express.static(resolve(__dirname, "..", "uploads")));
+
+app.use(errors());
 
 app.listen(3333);
